6955d845a8d142b351350bfe06430c623436ced3
Jinhoo Bong <jinhoobong@gmail.com>
2020-06-16 21:48:52 -0700

    Initial set up process. Homepage set up

---
+++ src/Components/Homepage/homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Homepage.module.css';
+
+const homepage = () => {
+    return(
+        <div>
+            <h1 className={classes.Test}>Weather</h1>
+        </div>
+    )
+}
+
+export default homepage;
