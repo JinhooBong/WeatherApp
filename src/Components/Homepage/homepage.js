4f5d3ae52784f7547276b62b37c43b6eaecd521e
Jinhoo Bong <jinhoobong@gmail.com>
2020-06-18 22:36:47 -0700

    Updated Homepage

--- src/Components/Homepage/homepage.js
+++ src/Components/Homepage/homepage.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import classes from './Homepage.module.css';
+// import Forecast from '../Forecast/Forecast';
 
 const homepage = () => {
     return(
         <div>
             <h1 className={classes.Test}>Weather</h1>
+            <form>
+                <div className={classes.InputBox}>
+                    <h2 className={classes.Title}>Enter your location: </h2>
+                    <input className={classes.Input} id="city" type="text" placeholder="Enter City Name"></input>
+                </div>
+            </form>
         </div>
     )
 }
